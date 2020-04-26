5c20d9dd574ead592924bad23650c13c990cae1f
Shwetha <shwethasri444@gmail.com>
2020-04-26 13:58:06 +0530

    optimised article search

--- elearnerapp/articleSearch.py
+++ elearnerapp/articleSearch.py
@@ -12,55 +12,8 @@ import operator
 import syllabus as s
 stopwords=stopwords.words('english')
 
-# urls = []
-# urls.append("http://www.economicsdiscussion.net/marketing-management/what-is-marketing-management/31788")
-# urls.append("http://www.economicsdiscussion.net/human-resource-management/role-of-human-resource-management/31737")
-# urls.append("http://www.economicsdiscussion.net/human-resource-management/definition-of-human-resource-management/31830")
-# urls.append("http://www.economicsdiscussion.net/human-resource-management/recruitment-and-selection-process/31594")
-# urls.append("https://www.tutorialspoint.com/management_principles/index.htm")
-# urls.append("https://www.tutorialspoint.com/marketing_management/index.htm")
-# urls.append("https://www.tutorialspoint.com/human_resource_management/index.htm")
-# urls.append("https://www.tutorialspoint.com/recruitment_and_selection/index.htm")
-# urls.append("https://www.managementstudyguide.com/all-subjects.htm")
 
-# links=[]
-# keywords=[]
-# for url in urls:
-# 	print(url)
-# 	req=Request(url,headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
-# 	html = urlopen(req).read()
-# 	soup = BeautifulSoup(html, 'lxml')
-# 	all_links = soup.find_all('a')
-
-# 	for link in all_links:
-# 	    if link.get("href"):
-# 	    	if link.get("href").startswith("/"):
-# 	    	    if url.startswith("https://www.tutorialspoint.com"):
-# 	    	    	# print("/".join(url.split("/")[:-2])+link.get("href"))
-# 	    	    	lk = "/".join(url.split("/")[:-2])+link.get("href")
-# 	    	    	links.append(lk)
-# 	    	    elif url.startswith("http://www.economicsdiscussion.net"):
-# 	    	    	# print("/".join(url.split("/")[:-2])+link.get("href"))
-# 	    	    	lk = "/".join(url.split("/")[:-1])+link.get("href")
-# 	    	    	links.append(lk)
-# 	    	    elif url.startswith("https://www.managementstudyguide.com"):
-# 	    	    	# print("/".join(url.split("/")[:-2])+link.get("href"))
-# 	    	    	lk = "/".join(url.split("/")[:-1])+link.get("href")
-# 	    	    	links.append(lk)
-
-# 	    	else:
-# 	    	    links.append(link.get("href"))
-# 	    	temp=link.get_text()
-# 	    	temp=temp.strip("\n")
-# 	    	keywords.append(temp)
-	   
-	    
-	
-
-# dictionary = dict(zip(links,keywords))
-# df=pd.DataFrame(list(dictionary.items()),columns=['Link','Description'])
-# df.to_csv("KB.csv",index=False)
-df=pd.read_csv('KB.csv')
+df=pd.read_csv('ArticleKB.csv')
 keywords=df['Description'].astype(str).values.tolist()
 links=df['Link'].astype(str).values.tolist()
 
@@ -109,6 +62,7 @@ def Asearch(subject,unit):
 			 		if value[0]<0.7:
 			 			break
 			 		titles.append(item)
+
 
 	return dict(zip(final_links,titles))
 	
